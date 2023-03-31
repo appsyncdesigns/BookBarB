82e14ea5922d340e5e8395b9f61a0669556b5df4
Asdesigns <info@asdesignsknr.com>
2023-03-31 16:01:25 +0530

    commit

--- New folder/src/environments/environment.ts
+++ New folder/src/environments/environment.ts
@@ -8,6 +8,6 @@
 */
 export const environment = {
   production: false,
-  baseUrl: 'https://appsyncdesigns.com/api/',
+  baseUrl: 'https://appsyncdesigns.github.io/BookBarB',
   imageUrl: 'https://appsyncdesigns.com/api/images/',
 };
