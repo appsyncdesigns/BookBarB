2a31dae9c2708dc42a02a1c0051a0c77e53e0fba
Asdesigns <info@asdesignsknr.com>
2023-04-12 12:20:46 +0530

    commit

--- New folder/src/app/pages/freelancer/freelancer.component.ts
+++ New folder/src/app/pages/freelancer/freelancer.component.ts
@@ -108,8 +108,8 @@ export class FreelancerComponent implements OnInit {
       this.api.uploadFile(files).subscribe((data: any) => {
         console.log('==>>>>>>', data.data);
         this.util.hide();
-        if (data && data.data.image_name) {
-          this.cover = data.data.image_name;
+        if (data && data.data) {
+          this.cover = data.data;
         }
       }, err => {
         console.log(err);
