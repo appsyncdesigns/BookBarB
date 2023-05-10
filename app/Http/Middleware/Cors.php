2fc7614158fb28643f5106f020df0d7d85134bd8
Asdesigns <info@asdesignsknr.com>
2023-05-10 16:55:11 +0530

    commit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,9 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+        header('Access-Control-Allow-Origin: *');
+        return $next($request);
     }
 }
