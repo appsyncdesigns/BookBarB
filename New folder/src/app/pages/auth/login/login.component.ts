5da70263ecc0116e68a8b454aff8319719f6599c
Asdesigns <info@asdesignsknr.com>
2023-05-19 10:54:13 +0530

    commit

--- New folder/src/app/pages/auth/login/login.component.ts
+++ New folder/src/app/pages/auth/login/login.component.ts
@@ -49,7 +49,7 @@ export class LoginComponent {
     this.api.post('v1/auth/adminLogin', param).then((data: any) => {
       console.log("+++++++++++++++", data);
       this.util.hide();
-      if (data && data.status && data.status == 200 && data.user && data.user.type == 'admin' || data.user.type == 'city') {
+      if (data && data.status && data.status == 200 && data.user && data.user.type == 'admin') {
         localStorage.setItem('uid', data.user.id);
         localStorage.setItem('token', data.token);
         this.router.navigate(['']);
